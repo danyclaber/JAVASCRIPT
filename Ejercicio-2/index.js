617c305cc97411a1b5cbdc03d4d2d057ff223591
Christian Medrano <christiandmc97@gmail.com>
2023-04-30 23:52:58 -0400

    Arrays, Object and Date

---
+++ Ejercicio-2/index.js
@@ -0,0 +1,9 @@
+lista = ["Christian Daniel Medrano Kantuta", "26 Años", true, new Date("March 3 1997"),
+    libro = {
+        titulo: "The Art of Computer Programming",
+        autor: "Donald Knuth",
+        fecha: 1968,
+        url: "https://www.amazon.com/Computer-Programming-Volumes-1-4A-Boxed/dp/0321751043",
+    }];
+console.log(lista);
+
