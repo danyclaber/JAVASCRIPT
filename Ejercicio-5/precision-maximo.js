48d3408524570bb089ab012f5c7757b1af24dcd4
Christian Medrano <christiandmc97@gmail.com>
2023-06-01 20:40:57 -0400

    Metodos de precision y maximo valor numerico en JS

---
+++ Ejercicio-5/precision-maximo.js
@@ -0,0 +1,12 @@
+
+let alturaCm = 173;
+let alturaMt = 1.73;
+let pesoKg = 61.5;
+
+console.log(alturaMt.toFixed(3));
+console.log(pesoKg.toFixed(0));
+
+let max_valor_JS = Number.MAX_VALUE;
+
+console.log(max_valor_JS +1 === max_valor_JS);
+
