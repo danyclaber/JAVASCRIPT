d892eb90e732fe1ef5cc1a5c2d8e4f7d394c7270
Christian Medrano <christiandmc97@gmail.com>
2023-06-07 22:53:37 -0400

    Conjuntos

---
+++ Ejercicio-7/Sets.js
@@ -0,0 +1,10 @@
+const array = ["Christian", "Daniel", "Nikol", "Ridert", "Edwin", "Rossmery"];
+const miSet = new Set(array);
+
+console.log(miSet);
+miSet.add("Christian");
+console.log(miSet);
+
+//ADD JAVASCRIPT
+miSet.add("Javascript");
+console.log(miSet);
